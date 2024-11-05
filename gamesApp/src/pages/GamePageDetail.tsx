2dd5933b1f3cf73d87e14601445df60f05501d04
kooroshFarv <farvardin.koorosh@gmail.com>
2024-11-05 16:29:39 -0500

    Layout improvement

--- gamesApp/src/pages/GamePageDetail.tsx
+++ gamesApp/src/pages/GamePageDetail.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import { useParams } from 'react-router-dom';
 import useGame from '../hooks/useGame';
-import { Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
+import { GridItem, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import ExpandableText from '../components/ExpandableText';
-import DefinitionItem from '../components/DefinitionItem';
 import GameAttributes from '../components/GameAttributes';
 import GameTrailers from '../components/GameTrailers';
 import Screenshots from '../components/Screenshots';
@@ -16,15 +14,17 @@ const GamePageDetail = () => {
   if(isLoading) return  <Spinner />;
   if(error || !game) throw error;
   return (
-    <>
-      <Heading >
-        {game.name}
-      </Heading>
+    <SimpleGrid columns={{base : 1, md: 2}} spacing={10}> 
+    <GridItem>
+      <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText> 
       <GameAttributes game={game}/>
+    </GridItem>
+    <GridItem>
       <GameTrailers gameId={game.id}/>
       < Screenshots gameId={game.id}/>
-    </>
+    </GridItem>
+    </SimpleGrid>
   )
 }
 
