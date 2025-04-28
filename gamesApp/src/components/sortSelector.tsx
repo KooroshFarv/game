a4e539559b7cbd223ef102812939cf3fb2274770
kooroshFarv <farvardin.koorosh@gmail.com>
2025-04-28 01:41:31 -0400

    sortSelector

--- gamesApp/src/components/sortSelector.tsx
+++ gamesApp/src/components/sortSelector.tsx
@@ -1,36 +1,42 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/menu'
+import { Button } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 
 interface Props {
-    onSelectSortOrder : (sortOrder : string) => void;
-    sortOrder : string
+  onSelectSortOrder: (sortOrder: string) => void
+  sortOrder: string
 }
 
-const sortSelector = ({onSelectSortOrder, sortOrder} : Props) => {
-    const sortorders = [
-        {value: '', label : 'Relevance'},
-        {value: '-added', label : 'Date added'}, 
-        {value: 'name', label : 'Name'},
-        {value: '-released', label : 'Release date'},
-        {value: '-metacritic', label : 'Popularity'},
-        {value: '-rating', label : 'Average Rate'},
-    ]
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: '', label: 'Relevance' },
+    { value: '-added', label: 'Date added' },
+    { value: 'name', label: 'Name' },
+    { value: '-released', label: 'Release date' },
+    { value: '-metacritic', label: 'Popularity' },
+    { value: '-rating', label: 'Average Rate' },
+  ]
 
-    const currentSortOrder = sortorders.find(order => order.value === sortOrder)
+  const currentSortOrder = sortOrders.find(order => order.value === sortOrder)
+
   return (
-    <Menu> 
-    <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-    Order by : {currentSortOrder?.label || 'Relevance'}
-    </MenuButton>
-    <MenuList>
-       {sortorders.map((order) =>(
-        <MenuItem onClick={() => onSelectSortOrder(order.value)} key={order.value} value={order.value}>
+    <Menu>
+      <MenuButton as={Button}>
+        Order by: {currentSortOrder?.label || 'Relevance'}
+        <BsChevronDown style={{ display: 'inline', marginLeft: '8px' }} />
+      </MenuButton>
+      <MenuList>
+        {sortOrders.map(order => (
+          <MenuItem
+            key={order.value}
+            onClick={() => onSelectSortOrder(order.value)}
+          >
             {order.label}
-        </MenuItem>
-    ))}
-    </MenuList>
-</Menu>
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
   )
 }
 
-export default sortSelector
+export default SortSelector
